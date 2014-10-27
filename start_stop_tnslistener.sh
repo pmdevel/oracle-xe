d1c83b3012f09df1d311ce6387e1295eea0fc183
nikahl <niclas.ahlstrand@pensionsmyndigheten.se>
2014-10-27 08:09:13 +0100

    Fixed error in shellscript

--- start_stop_tnslistener.sh
+++ start_stop_tnslistener.sh
@@ -2,7 +2,7 @@
 
 CMD=$1
 
-if [ "${CMD}"== "start" ]; then
+if [ "${CMD}" = "start" ]; then
 	su -l -c "source $ORACLE_HOME/bin/oracle_env.sh; lsnrctl start " oracle
 elif [ "${CMD}" = "start" ]; then
 	su -l -c "source $ORACLE_HOME/bin/oracle_env.sh; lsnrctl stop " oracle
